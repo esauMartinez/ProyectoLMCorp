f021e05b08c65650046b95ff1101682c64f1d1e0
esau <martinez117esau@gmail.com>
2019-10-15 17:06:40 -0500

    add icons menu

--- public/javascripts/unidades/menu.js
+++ public/javascripts/unidades/menu.js
@@ -9,7 +9,7 @@ let menu = function() {
             <div class="col-lg-6">
                 <div class="card mt-3">
                     <div class="mt-3 d-flex justify-content-center align-itemns-center">
-                        <i class="fa fa-truck" style="font-size: 64px;"></i>
+                        <i class="fa fa-clipboard-list" style="font-size: 64px;"></i>
                     </div>
                     <div class="card-body">
                         <div class="row d-flex justify-content-center">
@@ -23,7 +23,7 @@ let menu = function() {
             <div class="col-lg-6">
                 <div class="card mt-3" style="width: 100%;">
                     <div class="mt-3 d-flex justify-content-center align-itemns-center">
-                        <i class="fa fa-clipboard-list" style="font-size: 64px;"></i>
+                        <i class="fa fa-truck" style="font-size: 64px;"></i>
                     </div>
                     <div class="card-body">
                         <div class="row d-flex justify-content-center">
